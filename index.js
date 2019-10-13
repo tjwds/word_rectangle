c0880c07e87b02ca82d47aab91b0d2cffcf94dd9
Joe Woods <woodsja@vcu.edu>
2019-10-13 15:38:32 -0400

    finish implementation

--- index.js
+++ index.js
@@ -14,17 +14,11 @@ let wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n');
 wordArray = wordArray.sort();
 
 // pre-optimize:  longest set of more than one words is at length of 27.
-let longest_word = 27;
-// wordArray.forEach(word => {
-//   if (word.length > longest_word) {
-//     longest_word = word.length;
-//   }
-// })
-
-// console.log(longest_word)
+// let longest_word = 27;
+let longest_word = 5;
 
 const conditionalAccessAndAppend = (input_array, x, y, target) => {
-  // our 3d-ness might not be 3d enough yet, too. 
+  // our 3d-ness might not be 3d enough yet, too.
   if (typeof (input_array[x]) === "undefined") {
     // this stack overflow seems to be … very wrong.  I don't understand this yet.
     return true;
@@ -50,6 +44,7 @@ const conditionalAccessAndAppend = (input_array, x, y, target) => {
 
 
 const generatePossibleRectangles = (possible_word_length, result_rectangle) => {
+
   // this is a doubly nested array, so we need to make sure we're not transacting on it.
   var acting_rectangle = [...result_rectangle];
   var possible_rectangle = [...result_rectangle];
@@ -59,7 +54,8 @@ const generatePossibleRectangles = (possible_word_length, result_rectangle) => {
   possible_words = wordArray.filter(word => word.length === possible_word_length)
 
   // find a possible word for the acting rectangle.  add the word's letters to our acting rectangle.  if no word exists, return false.
-  acting_rectangle.forEach(acting_array => {
+  for (i = 0; i < acting_rectangle.length; i++) {
+    acting_array = acting_rectangle[i];
     // create a vertical testing word.
     vert_test_word = acting_array.join('');
     // does a word exist which starts with that vertical testing word?
@@ -76,21 +72,109 @@ const generatePossibleRectangles = (possible_word_length, result_rectangle) => {
         conditionalAccessAndAppend(possible_rectangle, x, y, possible_vert_letter)
       })
     })
-  })
+  }
+
   return possible_rectangle;
 }
 
-for (word_length = longest_word; word_length > 1; word_length--) {
-  // create an array of our possible seed words of this current length.
-  let seed_word_array = wordArray.filter(word => word.length === word_length);
+const getStartRowIndex = (possible_rectangles) => {
+  for (var i = 0; i < possible_rectangles[0].length; i++) {
+    if (typeof (possible_rectangles[0][i]) === "object") {
+      return i;
+    }
+  }
 
-  // do we have more than one word of this length? 
-  if (seed_word_array.length < 2) {
-    console.log(`Seed word array length was ` + seed_word_array.length + ` for size ` + word_length)
+  return -1;
+}
+
+const generateWordRectangle = (possible_rectangles, x_word_array, y_word_array, possible_word_length) => {
+  // what's the first row that we haven't solved for yet?
+  start_row_index = getStartRowIndex(possible_rectangles)
+
+  // if we somehow make it all the way down and every word is locked in, we have a word square!!! return it.
+  if (start_row_index === -1) {
+    return possible_rectangles
+  }
+
+  // make a list of possible words that will fit in this row.
+  this_row_words = [...x_word_array];
+
+  // get this row's exact testing conditions. [["a"], ["b", "c"], ["d", "e"]]
+  testing_row = possible_rectangles.map(row => row[start_row_index])
+  // does any word fit?
+  this_row_words = this_row_words.filter(word => {
+    var word_letters = [...word]
+
+    for (i = 0; i < word_letters.length; i++) {
+      var word_letter = word_letters[i];
+
+      if (!testing_row[i].includes(word_letter)) {
+        return false;
+      }
+    }
+
+    return true;
+  })
+
+  // do we not have any word candidate for this row?  if not, go on to the next word.
+  if (!this_row_words.length) {
+    return false;
+  }
+
+  // for each word that will fit in this row...
+  for (i = 0; i < this_row_words.length; i++) {
+    row_candidate = this_row_words[i]
+
+    // filter out our possible letters in the next rows based on the words remaining in each column.
+    row_candidate_letters = [...row_candidate];
+    var acting_possible_rectangles = [...possible_rectangles];
+    acting_possible_rectangles = acting_possible_rectangles.map(_ => [..._])
+    row_candidate_letters.forEach((row_candidate_letter, index) => {
+      acting_possible_rectangles[index][start_row_index] = row_candidate_letter;
+    })
+
+    // generate the list of first letters [ [ 'a', [], [] ], [ 'a' ], [ 'h' ] ]
+    var first_letter_list = []
+    acting_possible_rectangles.forEach(row => {
+      this_column = '';
+      row.forEach(column => {
+        if (typeof (column) === "string") {
+          this_column += column
+        }
+      })
+      if (this_column) {
+        first_letter_list.push(this_column)
+      }
+    })
+
+
+    // if we run into any square that is now an array that is the length of zero, go on to the next word.
+    acting_possible_rectangles = generatePossibleRectangles(possible_word_length, first_letter_list);
+
+    // step down if there's not a possible rectangle for this word length.
+    if (!acting_possible_rectangles) {
+      continue;
+    }
+
+    // if we made it this far, recursively call generateWordRectangle with our new, locked-in word.  if that fails, go on to the next word.
+    return generateWordRectangle(acting_possible_rectangles, x_word_array, y_word_array, possible_word_length)
+  }
+
+  // if we've run out of words, return false so we can move on to the next possible rectangle.
+  return false;
+}
+
+for (word_length = longest_word; word_length > 2; word_length--) {
+  // create an array of our possible seed words of this current length.
+  let x_word_array = wordArray.filter(word => word.length === word_length);
+
+  // do we have more than one word of this length?
+  if (x_word_array.length < 2) {
+    console.log(`Seed word array length was ` + x_word_array.length + ` for size ` + word_length)
     continue;
   }
   // for every word in this list — our seed word...
-  seed_word_array.forEach(seed_word => {
+  x_word_array.forEach(seed_word => {
     // generate our 3d array of locked-in rectangle.
     result_rectangle = [...seed_word].map(seed_word_letter => [seed_word_letter])
 
@@ -105,8 +189,20 @@ for (word_length = longest_word; word_length > 1; word_length--) {
         continue;
       }
 
-      // now we have a single possible rectangle, it's time to see if there's a series of words that will fit in it. 
-      // TODO: does a grid exist which satisfies this set of letters? if so, it's one of the longest grids.  If not, step down.
+      // this is not great design, but we need to generate our y word array based on the length of our possible rectangle...
+      y_word_array_len = possible_rectangles[0].length;
+      y_word_array = wordArray.filter(word => word.length === y_word_array_len)
+
+      // now we have a single possible rectangle, it's time to see if there's a series of words that will fit in it.
+      var word_rectangle = generateWordRectangle(possible_rectangles, x_word_array, y_word_array, possible_word_length);
+
+      // does a grid exist which satisfies this set of letters? if so, it's one of the longest grids.
+      if (word_rectangle) {
+        word_rectangle.forEach(word => {
+          console.log(word.join(''))
+        })
+        console.log("---------")
+      }
     }
   })
 
